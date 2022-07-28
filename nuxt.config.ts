0654d60b64cde44d2427b5ccd3050035f887f304
Jerry Jäppinen <eiskis@gmail.com>
2022-07-28 20:17:46 +0200

    Minimal issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
+import { isArray } from 'lodash-es'
+
 import { defineNuxtConfig } from 'nuxt'
+
+console.log('all', typeof isArray)
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
